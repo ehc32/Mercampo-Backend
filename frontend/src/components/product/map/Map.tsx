22538a1efe340f5264026f7d1cfe9090a94ffd1e
Nicolás Cerquera Nieva <114252472+Nicolas0209x@users.noreply.github.com>
2024-08-21 16:48:17 -0500

    update

--- frontend/src/components/product/map/Map.tsx
+++ frontend/src/components/product/map/Map.tsx
@@ -1,21 +1,44 @@
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 import './styles.css'
+import { useEffect, useState } from 'react';
 
 interface locationStatus {
-    lat: number;
-    lng: number;
+    address: string;
     darkMode: boolean;
 }
 
-const Map: React.FC<locationStatus> = ({ lat, lng, darkMode }) => {
+const Map: React.FC<locationStatus> = ({ address, darkMode }) => {
 
-    const { isLoaded } = useJsApiLoader({
+    const { isLoaded, loadError } = useJsApiLoader({
         id: 'google-maps-script',
         googleMapsApiKey: 'AIzaSyDWmh4H4O1AqdP5-nzLJft-EdFo9m6TDk8' // tener en cuenta de que esto puede variar
     });
 
+    const [lat, setLat] = useState(0);
+    const [lng, setLng] = useState(0);
+
+    const geocodeAddress = async () => {
+        const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyDWmh4H4O1AqdP5-nzLJft-EdFo9m6TDk8`);
+        const data = await response.json();
+        console.log(data)
+        if (data.results.length > 0) {
+            const coordinates = data.results[0].geometry.location;
+            console.log(coordinates)
+            setLat(coordinates.lat);
+            setLng(coordinates.lng);
+        }
+    };
+
+    useEffect(() => {
+        geocodeAddress();
+    }, [address]);
+
+    if (loadError) {
+        return <div>Error al cargar el mapa</div>;
+    }
+
     if (!isLoaded) {
-        return <div>Loading...</div>;
+        return <div>Cargando...</div>;
     }
 
     return (
@@ -28,10 +51,16 @@ const Map: React.FC<locationStatus> = ({ lat, lng, darkMode }) => {
                 center={{ lat: lat, lng: lng }}
                 zoom={12}
             >
-                {/* Marcadores o otros componentes del mapa */}
+                {lat && lng && (
+                    <Marker
+                        position={{ lat: lat, lng: lng }}
+                        icon={{
+                            url: 'https://sena.edu.co/Style%20Library/alayout/images/logoSena.png',
+                            scaledSize: new window.google.maps.Size(40, 40),
+                        }}
+                    />
+                )}
             </GoogleMap>
-
-
         </section>
     );
 };
