be1c6f2a3c3b5fd96254e6bf6ced820bd7c82493
NicooWhite <juanchoescobarsosa@gmail.com>
2024-08-22 17:48:27 -0500

    adjusment login and register

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -7,7 +7,6 @@ import { useAuthStore } from "../hooks/auth";
 import MySwiper from "../components/shared/Swiper/swiper";
 
 const LoginPage = () => {
-
   const navigate = useNavigate();
   const { isAuth } = useAuthStore();
   const setToken = useAuthStore((state) => state.setToken);
@@ -18,74 +17,99 @@ const LoginPage = () => {
   const loginMutation = useMutation({
     mutationFn: () => loginRequest(email, password),
     onSuccess: (response) => {
-      setToken(response.data.access, response.data.refresh)
-      toast.success("Login exitoso!")
-      navigate("/")
+      setToken(response.data.access, response.data.refresh);
+      toast.success("Login exitoso!");
+      navigate("/");
     },
     onError: () => {
-      toast.error("Hubo un error, intenta devuelta")
-    }
-  })
+      toast.error("Hubo un error, intenta devuelta");
+    },
+  });
 
   const carrouselData = [
     {
-      foto: 'https://periodismopublico.com/wp-content/uploads/2019/06/Sena-.jpg'
+      foto: "https://periodismopublico.com/wp-content/uploads/2019/06/Sena-.jpg",
     },
     {
-      foto: 'https://www.elolfato.com/sites/default/files/styles/news_full_image/public/assets/news/foto-home-03022023.png?itok=OVxS2L5E',
+      foto: "https://www.elolfato.com/sites/default/files/styles/news_full_image/public/assets/news/foto-home-03022023.png?itok=OVxS2L5E",
     },
-  ]
+  ];
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    loginMutation.mutate()
-  }
+    event.preventDefault();
+    loginMutation.mutate();
+  };
 
-  if (loginMutation.isLoading) return <p>Loading...</p>
-  if (isAuth) return (<Navigate to="/" />)
+  if (loginMutation.isLoading) return <p>Loading...</p>;
+  if (isAuth) return <Navigate to="/" />;
 
   return (
-
-    <div className="flex flex-row items-center justify-center px-6 py-8 mx-auto md:h-[800px] lg:py-0">
-      {/* <Link to="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-        <img className="w-8 h-8 mr-2"
-          src="/logo.png"
-          alt="logo" />
-        <span>¡Bienvenidos!</span>
-      </Link> */}
-      <div className="w-full md:w-[400px] lg:w-[500px] bg-slate-300 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 flex flex-row">
-        <div className="p-6 space-y-4 md:space-y-6 sm:p-8 ">
+    <div className="flex flex-row items-center justify-center px-6 py-12 mx-auto md:h-[600px] lg:py-12">
+      <div className="w-full md:w-[80%] lg:w-[80%] bg-slate-300 rounded-lg shadow dark:border md:mt-0 xl:p-0 dark:bg-gray-800 dark:border-gray-700 flex flex-row">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-[35%]">
           <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
             Inicia sesión
           </h1>
           <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-
             <div>
-              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Correo electrónico</label>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Correo electrónico
+              </label>
               <input
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" />
+                type="email"
+                name="email"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-lime-600 focus:border-lime-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-lime-500 dark:focus:border-lime-500"
+                placeholder="name@company.com"
+              />
             </div>
 
             <div>
-              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Contraseña
+              </label>
               <input
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                type="password"
+                name="password"
+                id="password"
+                placeholder="••••••••"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-lime-600 focus:border-lime-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-lime-500 dark:focus:border-lime-500"
+              />
             </div>
 
-            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Ingresar</button>
+            <button
+              type="submit"
+              className="w-full text-white bg-lime-600 hover:bg-lime-700 focus:ring-4 focus:outline-none focus:ring-lime-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-lime-600 dark:hover:bg-lime-700 dark:focus:ring-lime-800"
+            >
+              Ingresar
+            </button>
             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-              No tienes cuenta? <Link to={'/register'} className="font-medium text-primary-600 hover:underline dark:text-primary-500">Registrate</Link>
+              No tienes cuenta?{" "}
+              <Link
+                to={"/register"}
+                className="font-medium text-lime-500 hover:underline dark:text-lime-600"
+              >
+                Registrate
+              </Link>
             </p>
           </form>
         </div>
-      <MySwiper width="90%" height="50vh" datos={carrouselData} />  
+
+        <div className="w-[65%]">
+          <MySwiper width="100%" height="60vh" datos={carrouselData} />
+        </div>
       </div>
     </div>
-  )
-
-}
+  );
+};
 export default LoginPage;
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -4,6 +4,7 @@ import { toast } from "react-hot-toast";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import { registerRequest } from "../api/users";
 import { useAuthStore } from "../hooks/auth";
+import MySwiper from "../components/shared/Swiper/swiper";
 
 const RegisterPage = () => {
 
@@ -35,6 +36,16 @@ const RegisterPage = () => {
     }
   }
 
+  const carrouselData = [
+    {
+      foto: "https://periodismopublico.com/wp-content/uploads/2019/06/Sena-.jpg",
+    },
+    {
+      foto: "https://www.elolfato.com/sites/default/files/styles/news_full_image/public/assets/news/foto-home-03022023.png?itok=OVxS2L5E",
+    },
+  ];
+
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     if (password !== re_password) {
@@ -48,77 +59,124 @@ const RegisterPage = () => {
   if (isAuth) return (<Navigate to="/" />)
 
   return (
-    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-[800px] lg:py-0">
-      <Link to="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-        <img className="w-8 h-8 mr-2"
-          src="/logo.png"
-          alt="logo" />
-        <span>¡Registrate!</span>
-      </Link>
-      <div className="w-full md:w-[400px] lg:w-[500px] bg-slate-300 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-            Crear nueva cuenta
-          </h1>
-          <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-
-            <div>
-              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Correo electrónico</label>
-              <input
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                type="email"
-                required
-                name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Correo electrónico" />
-            </div>
-
-            <div>
-              <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
-              <input
-                value={name}
-                required
-                onChange={(e) => setName(e.target.value)}
-                type="name" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nombre" />
-            </div>
-
-            <div>
-              <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Apellido</label>
-              <input
-                value={last_name}
-                required
-                onChange={(e) => setLastName(e.target.value)}
-                type="last_name" name="last_name" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Apellido" />
-            </div>
-
-            <div>
-              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
-              <input
-                value={password}
-                required
-                onChange={(e) => setPassword(e.target.value)}
-                type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-            </div>
-
-            <div>
-              <label htmlFor="re-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Repite la contraseña</label>
-              <input
-                value={re_password}
-                required
-                onChange={(e) => setRePassword(e.target.value)}
-                type="password" name="re-password" id="re-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-            </div>
-
-            {handleMatch() ? false : <p className="text-sm font-medium text-red-500">Contraseñas no coinciden</p>}
-
-            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Ingresar</button>
-            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-              Ya tienes una cuenta? <Link to={'/login'} className="font-medium text-primary-600 hover:underline dark:text-primary-500">Ingresar</Link>
-            </p>
-          </form>
-        </div>
+    <div className="flex flex-row items-center justify-center px-6 py-12 mx-auto md:h-[800px] lg:py-12">
+    <div className="w-full md:w-[80%] lg:w-[80%] bg-slate-300 rounded-lg shadow dark:border md:mt-0 xl:p-0 dark:bg-gray-800 dark:border-gray-700 flex flex-row">
+      {/* Comentario: El contenedor se expande para ocupar el 80% del ancho de la página */}
+  
+      <div className="w-[65%]">
+        {/* Comentario: MySwiper ocupa el 65% del contenedor */}
+  
+        <MySwiper width="100%" height="60vh" datos={carrouselData} />
+        {/* Comentario: Ajustamos la altura a `60vh` para que se adapte al nuevo tamaño */}
+      </div>
+  
+      <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-[35%]">
+        {/* Comentario: El formulario ocupa el 35% del contenedor */}
+  
+        {/* <Link to="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <img className="w-8 h-8 mr-2" src="/logo.png" alt="logo" />
+          <span>¡Registrate!</span>
+        </Link> */}
+        <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+          Crear nueva cuenta
+        </h1>
+        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Correo electrónico
+            </label>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              required
+              name="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-lime-600 focus:border-lime-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-lime-500 dark:focus:border-lime-500"
+              placeholder="Correo electrónico"
+            />
+          </div>
+  
+          <div>
+            <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Nombre
+            </label>
+            <input
+              value={name}
+              required
+              onChange={(e) => setName(e.target.value)}
+              type="name"
+              name="name"
+              id="name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-lime-600 focus:border-lime-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-lime-500 dark:focus:border-lime-500"
+              placeholder="Nombre"
+            />
+          </div>
+  
+          <div>
+            <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Apellido
+            </label>
+            <input
+              value={last_name}
+              required
+              onChange={(e) => setLastName(e.target.value)}
+              type="last_name"
+              name="last_name"
+              id="last_name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-lime-600 focus:border-lime-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-lime-500 dark:focus:border-lime-500"
+              placeholder="Apellido"
+            />
+          </div>
+  
+          <div>
+            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Contraseña
+            </label>
+            <input
+              value={password}
+              required
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              name="password"
+              id="password"
+              placeholder="••••••••"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-lime-600 focus:border-lime-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-lime-500 dark:focus:border-lime-500"
+            />
+          </div>
+  
+          <div>
+            <label htmlFor="re-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Repite la contraseña
+            </label>
+            <input
+              value={re_password}
+              required
+              onChange={(e) => setRePassword(e.target.value)}
+              type="password"
+              name="re-password"
+              id="re-password"
+              placeholder="••••••••"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-lime-600 focus:border-lime-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-lime-500 dark:focus:border-lime-500"
+            />
+          </div>
+  
+          {handleMatch() ? false : <p className="text-sm font-medium text-red-500">Contraseñas no coinciden</p>}
+  
+          <button
+            type="submit"
+            className="w-full text-white bg-lime-600 hover:bg-lime-700 focus:ring-4 focus:outline-none focus:ring-lime-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-lime-600 dark:hover:bg-lime-700 dark:focus:ring-lime-800"
+          >
+            Registrarse
+          </button>
+          <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+            Ya tienes una cuenta? <Link to={'/login'} className="font-medium text-lime-500 hover:underline dark:text-lime-600">Ingresar</Link>
+          </p>
+        </form>
       </div>
     </div>
-  )
+  </div>
+  )  
 
 }
 export default RegisterPage;
