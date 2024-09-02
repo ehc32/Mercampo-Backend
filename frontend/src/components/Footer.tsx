201cb7de8332d2213368764c0a67396d0dfb8efc
NicooWhite <juanchoescobarsosa@gmail.com>
2024-09-02 16:20:39 -0500

    register

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { FaFacebook, FaLinkedin, FaTwitter, FaWhatsapp, FaYoutube } from 'react-icons/fa';
 
 const Footer = () => {
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -42,9 +42,9 @@ const RegisterPage = () => {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="flex-grow flex justify-center px-4 pt-24 pb-6 mx-auto fondo-login">
-        <div className="w-full max-w-md bg-slate-300 bg-opacity-20 backdrop-filter backdrop-blur-md rounded-lg shadow dark:border dark:bg-gray-800 dark:bg-opacity-20 dark:backdrop-blur-md dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-4 sm:p-8 w-full">
+      <div className="flex-grow flex flex-col justify-center px-4 pt-20 pb-10 mx-auto fondo-login">
+        <div className="w-full max-w-md bg-slate-300 bg-opacity-20 backdrop-filter backdrop-blur-md rounded-lg shadow dark:border dark:bg-gray-800 dark:bg-opacity-20 dark:backdrop-blur-md dark:border-gray-700 mx-auto">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-black md:text-2xl dark:text-gray-100">
               Crear nueva cuenta
             </h1>
@@ -67,46 +67,45 @@ const RegisterPage = () => {
                   placeholder="Correo electrónico"
                 />
               </div>
-
-              <div className="flex flex-col md:flex-row md:space-x-4">
-                <div className="flex-1">
-                  <label
-                    htmlFor="name"
-                    className="block mb-2 text-sm font-bold text-black dark:text-gray-200"
-                  >
-                    Nombre
-                  </label>
-                  <input
-                    value={name}
-                    required
-                    onChange={(e) => setName(e.target.value)}
-                    type="text"
-                    name="name"
-                    id="name"
-                    className="focus:outline-none border border-gray-300 text-black font-bold sm:text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
-                    placeholder="Nombre"
-                  />
-                </div>
-                <div className="flex-1">
-                  <label
-                    htmlFor="phone"
-                    className="block mb-2 text-sm font-bold text-black dark:text-gray-200"
-                  >
-                    Teléfono
-                  </label>
-                  <input
-                    value={phone}
-                    required
-                    onChange={(e) => setPhone(e.target.value)}
-                    type="tel"
-                    name="phone"
-                    id="phone"
-                    className="focus:outline-none border border-gray-300 text-black font-bold sm:text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
-                    placeholder="Teléfono"
-                  />
-                </div>
+  
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block mb-2 text-sm font-bold text-black dark:text-gray-200"
+                >
+                  Nombre
+                </label>
+                <input
+                  value={name}
+                  required
+                  onChange={(e) => setName(e.target.value)}
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="focus:outline-none border border-gray-300 text-black font-bold sm:text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
+                  placeholder="Nombre"
+                />
               </div>
-
+  
+              <div>
+                <label
+                  htmlFor="phone"
+                  className="block mb-2 text-sm font-bold text-black dark:text-gray-200"
+                >
+                  Teléfono
+                </label>
+                <input
+                  value={phone}
+                  required
+                  onChange={(e) => setPhone(e.target.value)}
+                  type="tel"
+                  name="phone"
+                  id="phone"
+                  className="focus:outline-none border border-gray-300 text-black font-bold sm:text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
+                  placeholder="Teléfono"
+                />
+              </div>
+  
               <div>
                 <label
                   htmlFor="password"
@@ -125,7 +124,7 @@ const RegisterPage = () => {
                   className="border focus:outline-none border-gray-300 text-black font-bold sm:text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
                 />
               </div>
-
+  
               <div>
                 <label
                   htmlFor="re-password"
@@ -144,6 +143,7 @@ const RegisterPage = () => {
                   className="border focus:outline-none border-gray-300 text-black font-bold sm:text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
                 />
               </div>
+  
               <button
                 type="submit"
                 className="w-full text-white bg-[#39A900] hover:bg-lime-700 focus:ring-4 focus:outline-none focus:ring-lime-300 rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#39A900] dark:hover:bg-lime-700 dark:focus:ring-lime-800"
@@ -154,7 +154,7 @@ const RegisterPage = () => {
           </div>
         </div>
       </div>
-      <Footer /> {/* Oculta el footer para que no haya espacio vacío debajo del formulario */}
+      <Footer />
     </div>
   );
 };
