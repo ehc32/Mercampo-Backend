94da6afb52c39460d868796954217236d24b9f22
NicooWhite <juanchoescobarsosa@gmail.com>
2024-09-10 16:44:10 -0500

    update

--- frontend/src/pages/UserProfile.tsx
+++ frontend/src/pages/UserProfile.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { toast } from 'react-toastify';
-import Pagination from '@mui/material/Pagination';
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import Pagination from "@mui/material/Pagination";
 import {
   Avatar,
   Box,
@@ -17,14 +17,17 @@ import {
   TableHead,
   TableRow,
   Button,
-  TextField
-} from '@mui/material';
-import { Search } from 'lucide-react';
-import { get_solo_user, edit_user } from '../api/users';
-import { get_orders, get_order_items } from '../api/orders';
-import { useAuthStore } from '../hooks/auth';
-import { get_all_products_by_user, get_products_in_sells_by_user } from '../api/products';
-import ProfileTables from '../components/profile/profileTables';
+  TextField,
+} from "@mui/material";
+import { Search } from "lucide-react";
+import { get_solo_user, edit_user } from "../api/users";
+import { get_orders, get_order_items } from "../api/orders";
+import { useAuthStore } from "../hooks/auth";
+import {
+  get_all_products_by_user,
+  get_products_in_sells_by_user,
+} from "../api/products";
+import ProfileTables from "../components/profile/profileTables";
 
 const UserProfile = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -41,18 +44,18 @@ const UserProfile = () => {
   const { access: token } = useAuthStore();
   const id = token ? JSON.parse(atob(token.split(".")[1])).user_id : null;
 
-  const { data: user, isLoading, isError } = useQuery(
-    ["users", id],
-    () => get_solo_user(id),
-    {
-      enabled: !!id,
-    }
-  );
+  const {
+    data: user,
+    isLoading,
+    isError,
+  } = useQuery(["users", id], () => get_solo_user(id), {
+    enabled: !!id,
+  });
 
   function formatearFecha(fechaISO: any) {
     const fecha = new Date(fechaISO);
-    const dia = fecha.getDate().toString().padStart(2, '0'); // Asegura dos dígitos
-    const mes = (fecha.getMonth() + 1).toString().padStart(2, '0'); // Suma 1 y asegura dos dígitos
+    const dia = fecha.getDate().toString().padStart(2, "0"); // Asegura dos dígitos
+    const mes = (fecha.getMonth() + 1).toString().padStart(2, "0"); // Suma 1 y asegura dos dígitos
     const year = fecha.getFullYear();
     return `${dia}-${mes}-${year}`;
   }
@@ -65,7 +68,7 @@ const UserProfile = () => {
 
   const get_sells_by_user2 = async (id: number) => {
     const response = await get_products_in_sells_by_user(id);
-    setMyProductsSells(response.data);  
+    setMyProductsSells(response.data);
     setDataLenght2(response.meta.count);
   };
 
@@ -95,7 +98,10 @@ const UserProfile = () => {
     <Box sx={{ maxWidth: "lg", mx: "auto", p: 6, mt: ".1em" }}>
       <Card className="bg-gray-50 border border-gray-200 rounded-lg p-6 mb-8 shadow-lg hover:shadow-xl transition-shadow duration-300">
         <CardContent>
-          <Typography variant="h4" className="titulo-sala-compra-light text-2xl font-semibold text-gray-800 mb-6">
+          <Typography
+            variant="h4"
+            className="titulo-sala-compra-light text-2xl font-semibold text-gray-800 mb-6"
+          >
             Perfil del usuario
           </Typography>
           <ProfileTables user={user} id={id} />
@@ -104,28 +110,61 @@ const UserProfile = () => {
 
       <Card className="bg-gray-50 border border-gray-200 rounded-lg p-6 shadow-lg">
         <CardContent>
-          <Typography variant="h4" className="titulo-sala-compra-light text-2xl font-semibold text-gray-800 mb-2">
+          <Typography
+            variant="h4"
+            className="titulo-sala-compra-light text-2xl font-semibold text-gray-800 mb-2"
+          >
             Registro de compraventa
           </Typography>
-          <Typography variant="subtitle1" className="sub-titulo-sala-compra-light text-gray-600 mb-6">
+          <Typography
+            variant="subtitle1"
+            className="sub-titulo-sala-compra-light text-gray-600 mb-6"
+          >
             Visualiza tus órdenes de productos
-            {user?.role === "seller" && <span className="text-[#39A900]"> o ventas que has realizado</span>}
-            {user?.role === "admin" && <span className="text-[#39A900]"> o ventas que has realizado</span>}
+            {user?.role === "seller" && (
+              <span className="text-[#39A900]">
+                {" "}
+                o ventas que has realizado
+              </span>
+            )}
+            {user?.role === "admin" && (
+              <span className="text-[#39A900]">
+                {" "}
+                o ventas que has realizado
+              </span>
+            )}
           </Typography>
 
           <div className="flex justify-between items-center py-4">
-           
             <Tabs
               value={tabValue}
               onChange={(_, newValue) => setTabValue(newValue)}
               textColor="primary"
               indicatorColor="primary"
-              sx={{ '.MuiTabs-indicator': { backgroundColor: '#39A900' } }}
-
+              sx={{ ".MuiTabs-indicator": { backgroundColor: "#39A900" } }}
             >
-              <Tab label="Compras" value="compras" sx={{ '&.Mui-selected': { color: '#39A900' } }} className='focus:outline-none' />
-              {user?.role !== "client" && <Tab label="Ventas" value="ventas" sx={{ '&.Mui-selected': { color: '#39A900' } }} className='focus:outline-none' />}
-              {user?.role !== "client" && <Tab label="Mis productos" value="productos" sx={{ '&.Mui-selected': { color: '#39A900' } }} className='focus:outline-none' />}
+              <Tab
+                label="Compras"
+                value="compras"
+                sx={{ "&.Mui-selected": { color: "#39A900" } }}
+                className="focus:outline-none"
+              />
+              {user?.role !== "client" && (
+                <Tab
+                  label="Ventas"
+                  value="ventas"
+                  sx={{ "&.Mui-selected": { color: "#39A900" } }}
+                  className="focus:outline-none"
+                />
+              )}
+              {user?.role !== "client" && (
+                <Tab
+                  label="Mis productos"
+                  value="productos"
+                  sx={{ "&.Mui-selected": { color: "#39A900" } }}
+                  className="focus:outline-none"
+                />
+              )}
             </Tabs>
           </div>
 
@@ -151,7 +190,9 @@ const UserProfile = () => {
                       <TableCell>{order.order_date}</TableCell>
                       <TableCell>{order.delivery_date}</TableCell>
                       <TableCell>
-                        <Button onClick={() => handleOpenOrderModal(order.id)}>Ver Productos</Button>
+                        <Button onClick={() => handleOpenOrderModal(order.id)}>
+                          Ver Productos
+                        </Button>
                       </TableCell>
                     </TableRow>
                   ))}
@@ -178,7 +219,9 @@ const UserProfile = () => {
                           <TableCell>{product.name}</TableCell>
                           <TableCell>${product.price}</TableCell>
                           <TableCell>{product.description}</TableCell>
-                          <TableCell>{formatearFecha(product.created)}</TableCell>
+                          <TableCell>
+                            {formatearFecha(product.created)}
+                          </TableCell>
                           <TableCell>{product.count_in_sells}</TableCell>
                         </TableRow>
                       ))
@@ -204,74 +247,81 @@ const UserProfile = () => {
               </>
             )}
 
-            {tabValue === "productos" && (<>
-
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Nombre</TableCell>
-                    <TableCell>Descripción</TableCell>
-                    <TableCell>Calificación</TableCell>
-                    <TableCell>Precio</TableCell>
-                    <TableCell>Cantidad restante</TableCell>
-                    <TableCell>Vendidos</TableCell>
-                    <TableCell>F. Creación</TableCell>
-                    <TableCell>F. Vencimiento</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {myProducts.map((product) => (
-                    <TableRow key={product.id}>
-                      <TableCell>{product.name}</TableCell>
-                      <TableCell>{product.description}</TableCell>
-                      <TableCell>{product.rating}</TableCell>
-                      <TableCell>${product.price}</TableCell>
-                      <TableCell>{product.count_in_stock}</TableCell>
-                      <TableCell>{product.count_in_sells}</TableCell>
-                      <TableCell className='w-32'>{formatearFecha(product.created)}</TableCell>
-                      <TableCell className='w-32'>{formatearFecha(product.fecha_limite)}</TableCell>
+            {tabValue === "productos" && (
+              <>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell  className="font-bold ">Nombre</TableCell>
+                      <TableCell>Descripción</TableCell>
+                      <TableCell>Calificación</TableCell>
+                      <TableCell>Precio</TableCell>
+                      <TableCell>Cantidad restante</TableCell>
+                      <TableCell>Vendidos</TableCell>
+                      <TableCell>F. Creación</TableCell>
+                      <TableCell>F. Vencimiento</TableCell>
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-              <div>
-                <Pagination
-                  count={Math.ceil(dataLenght / 10)}
-                  page={page}
-                  onChange={(event, value) => setPage(value)}
-                  className="flex flex-row w-full justify-center my-6"
-                />
-              </div>
-            </>
+                  </TableHead>
+                  <TableBody>
+                    {myProducts.map((product) => (
+                      <TableRow key={product.id}>
+                        <TableCell>{product.name}</TableCell>
+                        <TableCell>{product.description}</TableCell>
+                        <TableCell>{product.rating}</TableCell>
+                        <TableCell>${product.price}</TableCell>
+                        <TableCell>{product.count_in_stock}</TableCell>
+                        <TableCell>{product.count_in_sells}</TableCell>
+                        <TableCell className="w-32">
+                          {formatearFecha(product.created)}
+                        </TableCell>
+                        <TableCell className="w-32">
+                          {formatearFecha(product.fecha_limite)}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+                <div>
+                  <Pagination
+                    count={Math.ceil(dataLenght / 10)}
+                    page={page}
+                    onChange={(event, value) => setPage(value)}
+                    className="flex flex-row w-full justify-center my-6"
+                  />
+                </div>
+              </>
             )}
           </Box>
         </CardContent>
       </Card>
 
-      <Modal
-        open={isOrderModalOpen}
-        onClose={() => setIsOrderModalOpen(false)}
-      >
-        <Box sx={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          width: 400,
-          bgcolor: 'background.paper',
-          boxShadow: 24,
-          p: 4,
-        }}>
-          <Typography variant="h6" id="modal-title" className="text-xl font-bold mb-4">
+      <Modal open={isOrderModalOpen} onClose={() => setIsOrderModalOpen(false)}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography
+            variant="h6"
+            id="modal-title"
+            className="text-xl font-bold mb-4"
+          >
             Detalles de la Orden
           </Typography>
           <Table>
             <TableHead>
               <TableRow>
-                <TableCell>ID</TableCell>
-                <TableCell>Nombre</TableCell>
-                <TableCell>Cantidad</TableCell>
-                <TableCell>Precio</TableCell>
+                <TableCell className="font-bold ">ID</TableCell>
+                <TableCell className="font-bold ">Nombre</TableCell>
+                <TableCell className="font-bold ">Cantidad</TableCell>
+                <TableCell className="font-bold ">Precio</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
